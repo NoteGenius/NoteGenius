72061dc6a8c3b86a265a1aa31dc09b8063f570c3
Dhruv Arora <a_dhruv@outlook.com>
2024-02-24 13:12:41 -0800

    Bug fixes, better documentation, and more transition
    ready to merge

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -92,20 +92,26 @@ import './sidebar.css';
 // }
 
 export default function Home() {
-    const [isOpen, setIsOpen] = useState(false);
+    const [isOpen, setIsOpen] = useState(false); // state for the sidebar
 
     return (
         <main className="flex items-center justify-center h-screen bg-gradient-to-t from-gray-custom-light to-gray-custom">
 
-              {/* Navigation bar */}
-              <nav className="md:hidden fixed top-0 right-0 m-4" style={{ marginLeft: 'auto' }}>
+
+
+              {/* transitioning hamburger button (for mobile devices*/}
+              <nav className={`navbar ${isOpen ? 'open' : ''} md:hidden fixed top-0 right-0 m-4`}>
                 <button onClick={() => setIsOpen(!isOpen)}>
                   <img src="/hamburgerbutton.png" alt="Menu" className="w-8 h-8 mt-1.5 mr-4" />
                 </button>
               </nav>
 
-              {/* Sidebar that is reactive*/}
-              <div className={`sidebar ${isOpen ? 'open' : ''}`}> // uses the sidebar.css file
+
+
+
+
+              {/* Sidebar that is reactive uses sidebar.css*/}
+              <div className={`sidebar ${isOpen ? 'open' : ''} md:hidden`}>
                 <div className="fixed top-0 left-0 w-full h-full bg-[#1B1B1B] p-4 overflow-auto ">
 
 
@@ -131,7 +137,7 @@ export default function Home() {
                         Support
                       </a></li>
                       <li><a href="/subscribe" className="text-white flex -ml-1.5ww">
-                        <img src="/subscribe.webp" alt="Subscribe" className="h-10 w-10 mr-2 -mt-1.5" />
+                        <img src="/subscribe.webp" alt="Subscribe" className="h-10 w-10 mr-2 -mt-1.5 -ml-1.5" />
                         Subscribe
                       </a></li>
                       <li><a href="/settings" className="text-white flex">
@@ -143,15 +149,22 @@ export default function Home() {
                 </div>
               </div>
 
+
+
+
+              {/* Desktop top bar when not on mobile device */}
               <div className = "hidden md:flex">
                 <div className="fixed top-0 left-0 right-0 bg-[#1B1B1B] p-4">
                   <div className="flex items-center py-3 ml-5">
-                    {/* Desktop navigation bar */}
+
+                    {/* Text of NoteGenius with green background */}
                     <div className="relative flex place-items-center before:absolute before:h-[20px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[20px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-green-500 after:via-green-600 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-green-700 before:dark:opacity-100 after:dark:to-green-900 after:to-green-600 after:dark:opacity-100 before:lg:h-[20px] z-[-1]">
                       <div className="relative text-white text-4xl font-bold dark:drop-shadow-[0_0_0.3rem_#ffffff70]">
                         NoteGenius
                       </div>
                     </div>
+
+                    {/* Navigation components inside top bar */}
                     <nav className="hidden md:flex" style={{ marginLeft: 'auto' }}>
                       <ul className="flex space-x-4 text-xl underline">
                         <li><a href="/support" className="text-white px-4 flex">
