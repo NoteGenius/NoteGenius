dfdda097b99db6ee70130314762e6d2249bcf962
Dhruv Arora <a_dhruv@outlook.com>
2024-07-25 00:50:47 -0700

    Refactoring code structure core resides in src/app and UI is separate from src/app. Now using @/ for root directory notation in imports

---
+++ src/UI/Components/topbar.tsx
@@ -0,0 +1,137 @@
+import React from "react";
+import { useState } from "react";
+import "@/UI/Style/sidebar.css";
+
+const Topbar = () => {
+  const [isOpen, setIsOpen] = useState(false); // state for the sidebar
+
+  return (
+    <div>
+      {/* transitioning hamburger button (for mobile devices*/}
+      <nav
+        className={`navbar ${isOpen ? "open" : ""} md:hidden fixed top-0 right-0 m-4`}
+      >
+        <button onClick={() => setIsOpen(!isOpen)}>
+          <img
+            src="/hamburgerbutton.png"
+            alt="Menu"
+            className="w-8 h-8 mt-1.5 mr-4"
+          />
+        </button>
+      </nav>
+
+      {/* Sidebar that is reactive uses sidebar.css*/}
+      <div className={`sidebar ${isOpen ? "open" : ""} md:hidden`}>
+        <div className="fixed top-0 left-0 w-full h-full bg-[#1B1B1B] p-4 overflow-auto ">
+          {/* FasFabar for leaving the sidebar */}
+          <nav
+            className="md:hidden fixed right-0 top-0 m-4"
+            style={{ marginLeft: "auto" }}
+          >
+            <button onClick={() => setIsOpen(!isOpen)}>
+              <img
+                src="/hamburgerbutton.png"
+                alt="Menu"
+                className="w-8 h-8 mt-1.5 mr-4"
+              />
+            </button>
+          </nav>
+          <div className="flex flex-col items-center">
+            {/* Title of NoteGenius*/}
+            <div className="mt-12 mb-20 relative flex place-items-center before:absolute before:h-[20px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[20px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-green-500 after:via-green-600 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-green-700 before:dark:opacity-100 after:dark:to-green-900 after:to-green-600 after:dark:opacity-100 before:lg:h-[20px] z-[-1]">
+              <div className="relative text-white text-4xl font-bold dark:drop-shadow-[0_0_0.3rem_#ffffff70]">
+                NoteGenius
+              </div>
+            </div>
+
+            {/* Navigation components inside sidebar */}
+            <ul className="space-y-4 text-xl underline ">
+              <li>
+                <a href="/support" className="text-white flex">
+                  <img
+                    src="/support.png"
+                    alt="Support"
+                    className="h-8 w-8 mr-2 -mt-0.5"
+                  />
+                  Support
+                </a>
+              </li>
+              <li>
+                <a href="/subscribe" className="text-white flex -ml-1.5ww">
+                  <img
+                    src="/subscribe.webp"
+                    alt="Subscribe"
+                    className="h-10 w-10 mr-2 -mt-1.5 -ml-1.5"
+                  />
+                  Subscribe
+                </a>
+              </li>
+              <li>
+                <a href="/settings" className="text-white flex">
+                  <img
+                    src="/settings.png"
+                    alt="Settings"
+                    className="h-8 w-8 mr-2 -mt-0.5"
+                  />
+                  Settings
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      {/* Desktop top bar when not on mobile device */}
+      <div className="hidden md:flex">
+        <div className="fixed top-0 left-0 right-0 bg-[#1B1B1B] p-4">
+          <div className="flex items-center py-3 ml-5">
+            {/* Text of NoteGenius with green background */}
+            <div className="relative flex place-items-center before:absolute before:h-[20px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[20px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-green-500 after:via-green-600 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-green-700 before:dark:opacity-100 after:dark:to-green-900 after:to-green-600 after:dark:opacity-100 before:lg:h-[20px] z-[-1]">
+              <div className="relative text-white text-4xl font-bold dark:drop-shadow-[0_0_0.3rem_#ffffff70]">
+                NoteGenius
+              </div>
+            </div>
+
+            {/* Navigation components inside top bar */}
+            <nav className="hidden md:flex" style={{ marginLeft: "auto" }}>
+              <ul className="flex space-x-4 text-xl underline">
+                <li>
+                  <a href="/support" className="text-white px-4 flex">
+                    <img
+                      src="/support.png"
+                      alt="Support"
+                      className="h-8 w-8 mr-2 -mt-0.5"
+                    />
+                    Support
+                  </a>
+                </li>
+                <li>
+                  <a href="/subscribe" className="text-white px-4 flex">
+                    <img
+                      src="/subscribe.webp"
+                      alt="Subscribe"
+                      className="h-10 w-10 mr-2 -mt-1.5"
+                    />
+                    Subscribe
+                  </a>
+                </li>
+                <li>
+                  <a href="/settings" className="text-white px-4 flex">
+                    <img
+                      src="/settings.png"
+                      alt="Settings"
+                      className="h-8 w-8 mr-2 -mt-0.5"
+                    />
+                    Settings
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Topbar;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,37 @@
 "use client"; // client hook
-import Topbar from "./Components/topbar";
-import { useRef } from 'react';
+import Topbar from "@/UI/Components/topbar";
+import { useRef } from "react";
 
 export default function Home() {
   const textareaRef = useRef<HTMLTextAreaElement>(null);
 
   return (
     <main className="flex items-center justify-center h-screen bg-gradient-to-t from-gray-custom-light to-gray-custom">
-
       {/* Component for the Top bar of the main page */}
       <Topbar />
 
       {/* Typing bar */}
 
-      <div style={{width: '70vw'}} className="mx-auto flex bottom-2 absolute justify-center items-center p-4">
-        <button className="rounded-lg bottom-0 px-4 mr-4 bg-white">
+      <div
+        style={{ width: "70vw" }}
+        className="mx-auto flex bottom-2 absolute justify-center items-center p-4"
+      >
+        <button className="rounded-lg px-4 mr-4 bg-white">
           <img src="/plus.png" alt="Add" className="h-8 w-8" />
         </button>
-        <textarea 
+        <textarea
           ref={textareaRef}
           onInput={() => {
             if (textareaRef.current) {
-              textareaRef.current.style.height = 'auto';
+              textareaRef.current.style.height = "auto";
               textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`;
             }
           }}
-          style={{resize: 'none'}}
+          style={{ resize: "none" }}
           className="w-full rounded-lg px-4 py-2 bg-white text-black font-sans"
-          placeholder="Type here..." 
+          placeholder="Type here..."
         />
       </div>
-
     </main>
-  )
-    
+  );
 }
