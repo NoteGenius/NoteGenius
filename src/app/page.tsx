2cda09d2d55caffbcafc1a5aba3b8fd8f7d49a9a
Dhruv Arora <a_dhruv@outlook.com>
2024-07-25 00:55:05 -0700

    Removed typing bar for merge

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,30 +8,7 @@ export default function Home() {
   return (
     <main className="flex items-center justify-center h-screen bg-gradient-to-t from-gray-custom-light to-gray-custom">
       {/* Component for the Top bar of the main page */}
-      <Topbar />
-
-      {/* Typing bar */}
-
-      <div
-        style={{ width: "70vw" }}
-        className="mx-auto flex bottom-2 absolute justify-center items-center p-4"
-      >
-        <button className="rounded-lg px-4 mr-4 bg-white">
-          <img src="/plus.png" alt="Add" className="h-8 w-8" />
-        </button>
-        <textarea
-          ref={textareaRef}
-          onInput={() => {
-            if (textareaRef.current) {
-              textareaRef.current.style.height = "auto";
-              textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`;
-            }
-          }}
-          style={{ resize: "none" }}
-          className="w-full rounded-lg px-4 py-2 bg-white text-black font-sans"
-          placeholder="Type here..."
-        />
-      </div>
+      <Topbar /> 
     </main>
   );
 }
