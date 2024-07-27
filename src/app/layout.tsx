0231e7f9e5a8714bb6baf9c48c25798bdf5370cc
Dhruv Arora <a_dhruv@outlook.com>
2024-07-27 00:47:10 -0700

    Formatted

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,24 +5,24 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "NoteGenius",
-  icons: [
-    {
-      rel: "icon",
-      url: "/favicon.ico",
-    },
-  ],
-  description: "Your personal study guide from the future",
+    title: "NoteGenius",
+    icons: [
+        {
+            rel: "icon",
+            url: "/favicon.ico",
+        },
+    ],
+    description: "Your personal study guide from the future",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body className={inter.className}>{children}</body>
+        </html>
+    );
 }
