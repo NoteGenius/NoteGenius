750d16c84c25e2e1577e48a07a35550b889c703d
Dhruv Arora <a_dhruv@outlook.com>
2024-07-26 17:27:45 -0700

    Added changes to fix build error from vercel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,8 @@ export default function Home() {
   return (
     <div>
       <HomePage />
-      // Google Analytics
+
+      {/* Google Analytics */}
       <GoogleAnalytics
         trackPageViews
         strategy="lazyOnload"
