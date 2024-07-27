ee4f709c799586a4abda5a4289922ee983e646a7
Dhruv Arora <a_dhruv@outlook.com>
2024-07-26 19:19:33 -0700

    Added new user logging

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,34 @@
 "use client";
 import HomePage from "@/pages/homepage";
-import { GoogleAnalytics } from "nextjs-google-analytics";
+import { GoogleAnalytics, event } from "nextjs-google-analytics";
+import { useEffect } from "react";
+
+const handleNewUser = () => {
+    event('first_open', {
+      category: 'User',
+      action: 'New User',
+      value: 1,
+    });
+  };
 
 export default function Home() {
+  
+  useEffect(() => {
+    // Check if the user is new
+    const isNewUser = !localStorage.getItem('isNewUser');
+
+    // event handling
+    if (isNewUser !== false) {
+      localStorage.setItem('isNewUser', 'false');
+    
+      handleNewUser();
+      
+      // save that they are a new user
+      localStorage.setItem('isNewUser', 'true');
+    }
+  }, []);
+
+
   return (
     <div>
       <HomePage />
