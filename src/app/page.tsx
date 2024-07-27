950ee4dc9e688aae580b8b1c98e3df2e685e8e25
Dhruv Arora <a_dhruv@outlook.com>
2024-07-27 00:37:23 -0700

    Removed analytics events

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,8 @@
 "use client";
 import HomePage from "@/pages/homepage";
-import { GoogleAnalytics, event } from "nextjs-google-analytics";
-import { useEffect } from "react";
-
-const handleNewUser = () => {
-    event('first_open', {
-      category: 'User',
-      action: 'New User',
-      value: 1,
-    });
-  };
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
 export default function Home() {
-  
-  useEffect(() => {
-    // Check if the user is new
-    const isNewUser = !localStorage.getItem('isNewUser');
-
-    // event handling
-    if (isNewUser !== false) {
-      localStorage.setItem('isNewUser', 'false');
-    
-      handleNewUser();
-      
-      // save that they are a new user
-      localStorage.setItem('isNewUser', 'true');
-    }
-  }, []);
-
 
   return (
     <div>
