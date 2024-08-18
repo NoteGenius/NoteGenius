28fb5170eb15c2bd4c67fc5b0e4d12918fe22a26
Dhruv A <dhruvarora202520@gmail.com>
2024-08-18 14:50:03 -0700

    Working textbar need to add ability to hide when navbar is open. Will do with a context

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 "use client";
+import Textbar from "@/UI/Components/Textbar";
 import Topbar from "@/UI/Components/Topbar";
-
-// client hook
 
 /** Main page code */
 export default function Home() {
     return (
         <main className="flex items-center justify-center h-screen bg-gradient-to-t from-gray-custom-light to-gray-custom">
             <Topbar />
+            <Textbar />
         </main>
     );
 }
