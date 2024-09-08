4929e3865d01dd2c4ef8c586e46b98b7a833795a
Dhruv Arora <a_dhruv@outlook.com>
2024-09-08 11:27:32 -0700

    Working git build

--- src/UI/Components/Sources.tsx
+++ src/UI/Components/Sources.tsx
@@ -18,8 +18,21 @@ const Sources = () => {
     const [selectedSource, setSelectedSource] = useState("file"); // Default is file attachment
     const [selectedFiles, setSelectedFiles] = useState<File[]>([]); // Selected files for upload
 
+    if (typeof Promise.withResolvers === 'undefined') {
+        if (window)
+            // @ts-expect-error This does not exist outside of polyfill which this is doing
+            window.Promise.withResolvers = function () {
+                let resolve, reject;
+                const promise = new Promise((res, rej) => {
+                    resolve = res;
+                    reject = rej;
+                });
+                return { promise, resolve, reject };
+            };
+    }
+
     pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-    'pdfjs-dist/build/pdf.worker.min.mjs',
+    'pdfjs-dist/legacy/build/pdf.worker.min.mjs',
     import.meta.url,
     ).toString(); // Set the worker source for the pdf reader
 
