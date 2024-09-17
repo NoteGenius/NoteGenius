6554b865ba5cd229d0a315eaa6135103fcc6ed04
Dhruv Arora <dhruv.arora1@autodesk.com>
2024-09-17 11:20:05 -0700

    Added under construction to all pages that had 404 error

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,10 +1,5 @@
 import Construction from "@/UI/Components/Construction";
 
-/**
- * Settings Page
- *
- * Currently displays under construction
- */
 export default function SettingsPage() {
     return <Construction />;
 }
